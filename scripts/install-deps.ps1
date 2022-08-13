1beac6d830cb8da6aee65b94c68beb23e1108681
abdfnx <abdfnabd@gmail.com>
2022-08-13 05:23:34 +0300

    create windows deps installer

---
+++ scripts/install-deps.ps1
@@ -0,0 +1,16 @@
+# tgbot-cpp
+
+git clone https://github.com/reo7sp/tgbot-cpp
+cd tgbot-cpp
+cmake .
+make -j4
+make install
+cd ..
+
+# nlohmann-json
+git clone https://github.com/nlohmann/json
+cd json
+cmake .
+make -j
+make install
+cd ..
