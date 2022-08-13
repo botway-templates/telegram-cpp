ce1f723ce6afc632b6cd8f0570ace3d426f5daed
abdfnx <abdfnabd@gmail.com>
2022-08-13 05:15:03 +0300

    finish from `telegram-cpp` botway template

---
+++ scripts/install-deps.sh
@@ -0,0 +1,16 @@
+# tgbot-cpp
+
+git clone https://github.com/reo7sp/tgbot-cpp
+cd tgbot-cpp
+cmake .
+make -j4
+sudo make install
+cd ..
+
+# nlohmann-json
+git clone https://github.com/nlohmann/json
+cd json
+cmake .
+make -j
+sudo make install
+cd ..
